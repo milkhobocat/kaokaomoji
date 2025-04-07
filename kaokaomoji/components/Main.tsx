41d887e002cbef0b8018dbc9ede6242ed21462da
milk <86233625+milkhobocat@users.noreply.github.com>
2025-04-07 17:32:11 -0400

    Test fix bug
    removed animation from MagicButton + reverted size of svg

--- kaokaomoji/components/Main.tsx
+++ kaokaomoji/components/Main.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 
+
 const Main = () => {
   return (
     <main className="relative p-4 flex-col justify-center">
@@ -8,14 +9,13 @@ const Main = () => {
         <img
           src="/Kaokaomoji_title.svg"
           alt="KAOKAOMOJI Title"
-          className="mx-auto max-w-full h-auto"
+          className="mx-auto w-[900px] h-auto"
         />
       </div>
       <br />
-      <p className="font-bold p-4 text-center relative text-sm sm:text-base md:text-lg lg:text-xl text-[#ff9ca3] drop-shadow-[0_0_2px_#ffffff,0_0_10px_#ffffff]">
-        .☆ ݁˖Browse through the categories and click on a kaomoji to copy.☆ ݁˖
+      <p className="font-bold p-4 text-center relative text-base sm:text-base md:text-base text-[#ff9ca3] drop-shadow-[0_0_2px_#ffffff,0_0_10px_#ffffff]">
+      .☆ ݁˖Browse through the categories and click on a kaomoji to copy.☆ ݁˖
       </p>
-
       <br />
     </main>
   );
--- kaokaomoji/components/ui/MagicButton.tsx
+++ kaokaomoji/components/ui/MagicButton.tsx
@@ -1,6 +1,5 @@
-"use client"
-import React from "react";
-import { useState } from "react";
+"use client";
+import React, { useState } from "react";
 
 type MagicButtonProps = {
   children: React.ReactNode;
@@ -12,18 +11,15 @@ const MagicButton = ({ children }: MagicButtonProps) => {
   const handleClick = () => {
     navigator.clipboard.writeText(children as string);
     setCopied(true);
-    setTimeout(() => setCopied(false), 1530); // Reset after 1.5s
+    setTimeout(() => setCopied(false), 1500); // Reset after 1.5s
   };
 
   return (
     <button
       onClick={handleClick}
-      className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-[#f7849b] focus:ring-offset-2 focus:ring-offset-slate-50"
+      className="inline-flex h-12 items-center justify-center rounded-full bg-[#a5a4ed] px-3 py-1 text-sm font-medium text-white border-2 border-[#f7849b]"
     >
-      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#f54266_0%,#E2CBFF_50%,#f54266_100%)]" />
-      <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-[#a5a4ed] px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-        {copied ? "Copied!" : children}
-      </span>
+      {copied ? "Copied!" : children}
     </button>
   );
 };
