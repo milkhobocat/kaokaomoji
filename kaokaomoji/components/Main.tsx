e8b932fbaa941018beaddf68a3b409346d956a77
milk <86233625+milkhobocat@users.noreply.github.com>
2025-04-07 17:23:06 -0400

    Testing fix bug
    testing if it will fix mobile crash

--- kaokaomoji/components/Main.tsx
+++ kaokaomoji/components/Main.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 
-
 const Main = () => {
   return (
     <main className="relative p-4 flex-col justify-center">
@@ -9,13 +8,14 @@ const Main = () => {
         <img
           src="/Kaokaomoji_title.svg"
           alt="KAOKAOMOJI Title"
-          className="mx-auto w-[900px] h-auto"
+          className="mx-auto max-w-full h-auto"
         />
       </div>
       <br />
-      <p className="font-bold p-4 text-center relative text-base sm:text-base md:text-base text-[#ff9ca3] drop-shadow-[0_0_2px_#ffffff,0_0_10px_#ffffff]">
-      .☆ ݁˖Browse through the categories and click on a kaomoji to copy.☆ ݁˖
+      <p className="font-bold p-4 text-center relative text-sm sm:text-base md:text-lg lg:text-xl text-[#ff9ca3] drop-shadow-[0_0_2px_#ffffff,0_0_10px_#ffffff]">
+        .☆ ݁˖Browse through the categories and click on a kaomoji to copy.☆ ݁˖
       </p>
+
       <br />
     </main>
   );
